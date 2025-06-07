4334b7f0fabb95648168ed1d9433c5da7a34239f
Oslt-amira <107197253+Oslt-amira@users.noreply.github.com>
2025-06-07 01:35:12 +0100

    Eslint fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div className="bg-blue-500 xs:bg-red-100! p-4">Test XS Breakpoint</div>
-  );
+  return <div>Hi ! Please navigate to dashboard</div>;
 }
